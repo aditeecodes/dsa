cde52f3ab931a0e15ef954231d6f713650310153
aditeecodes <aditeecodes@gmail.com>
2026-06-06 23:07:24 +0530

    added new files

---
+++ dsa/reversingdigit.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+#include <math.h>
+ int main(){
+    int n;
+    cout <<"enter value of n";
+    cin >> n;
+    int ans =0;
+    while(n !=0){
+        int digit = n%10;
+        ans = ans *10 +digit;
+        n = n/10;
+
+    }
+    cout << ans;
+ }
